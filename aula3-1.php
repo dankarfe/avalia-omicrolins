004f83c2a735fc555ab0722ad10f3d726c25e42c
dankarfe <90410826+dankarfe@users.noreply.github.com>
2021-09-09 14:27:49 -0300

    Add files via upload

---
+++ aula3-1.php
@@ -0,0 +1,31 @@
+﻿<html>
+	<head>
+		<title>Título</title>
+	</head>
+	<body>
+		<table border="0" width=100% height=100%>
+			<tr height=15% align=center>
+				<td width=20% colspan=1 > <img src="img/olimpiadas.jpg"  width="150" height="60"></a> </td>
+					<td width=80% ><center><h1><b> Exercício de  HTML</h1></b></center> </td>
+			</tr>
+			<tr height=70% align=center>
+				<td width=100% colspan=4>Primeiro tempo
+Com Paulinho na vaga de Matheus Cunha, que não se recuperou para a partida,<br> a seleção brasileira modificou um pouco o estilo de jogo. Manteve a pressão no campo de defesa do México e conseguiu boas oportunidades até metade da primeira etapa.
+
+.
+
+A melhor delas em ótima jogada criada de uma ponta à outra até o corta-luz de Claudinho para Guilherme Arana<br> dominar e chutar para defesa de Ochoa. O goleiro mexicano ainda defenderia cobrança de falta de Daniel Alves e chute de Antony.
+
+.
+
+Sem muita saída, o México encontrou espaços nos erros do Brasil nos minutos finais e por pouco não abriu o placar.<br> Num lance com Romo e outro, dentro da área, de Antuna, em rápido contra-ataque - após erro de Claudinho no meio.</td>
+					
+			</tr>
+			<tr height=15% align=center>
+			<td width=20%> <img src="img/olimpiadas.jpg"  width="200" height="60"></a></td>
+				<td width=60%><h1>Layout com Tabela</h1></td>
+				<td width=20%> <img src="img/olimpiadas.jpg"  width="200" height="60"</a></td>
+			
+			</tr>
+		<table>
+	</body>
