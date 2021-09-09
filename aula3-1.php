82f679babf6b5d716c6959c9ffad99c7039b3da8
unknown <d.karim>
2021-09-09 14:44:17 -0300

    inclusão de texto

--- aula3-1.php
+++ aula3-1.php
@@ -12,7 +12,8 @@
 				<td width=100% colspan=4>Primeiro tempo
 					Em 2021, os Jogos Olímpicos serão no Estádio Olímpico de Tóquio, no Japão e, se o evento não for adiado ou cancelado, acontecerá de 23 de julho a 8 de agosto. <p>
 					Já as Paralimpíadas vão começar no dia 24 de agosto.<p>
-			</tr>
+					No último dia 8 de Agosto de 2021, as olimpíadas se encerraram. <p>
+					Com 17 dias de evento, esse torneio foi motivo de felicidade para o povo brasileiro, já que o Brasil trouxe um total de 21 medalhas para a casa: 7 de ouro, 6 de prata e 8 de bronze, algo nunca conquistado antes no nosso país.<p>
 			<tr height=15% align=center>
 			<td width=20%> <img src="img/olimpiadas.jpg"  width="200" height="60"></a></td>
 				<td width=60%><h1>Layout com Tabela</h1></td>
